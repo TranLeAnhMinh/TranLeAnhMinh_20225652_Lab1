99a814faa11b71682b1b22d6d49f84c9921b17fd
Trần Lê Anh Minh <165285045+TranLeAnhMinh@users.noreply.github.com>
2024-12-02 23:15:25 +0700

    Sửa lỗi

--- AimsProject/src/hust/soict/dsai/test/polymorphism/TestPolymorphism.java
+++ AimsProject/src/hust/soict/dsai/test/polymorphism/TestPolymorphism.java
@@ -5,14 +5,14 @@ import java.util.List;
 public class TestPolymorphism {
     public static void main(String[] args) {
         List<Media> mediae = new ArrayList<Media>();
-        DigitalVideoDisc dvd = new DigitalVideoDisc(1,"Neu ngay ay","Nhac tre", 18.5f,"Trinh Minh Duc",97);
+        DigitalVideoDisc dvd = new DigitalVideoDisc(1,"Neu ngay ay","Nhac tre", 18.5f,"Minh Tran",97);
         ArrayList<Track> tracks = new ArrayList<Track>();
         tracks.add(new Track("chuc mung ban",3));
         tracks.add(new Track("toi yeu ban",4));
-        CompactDisc cd = new CompactDisc(2,"mot minh anh noi dau","that tinh",25.5f,"Khanh Duy",tracks);
+        CompactDisc cd = new CompactDisc(2,"mot minh anh noi dau","that tinh",25.5f,"Anh Minh",tracks);
         List<String> authors = new ArrayList<String>();
         authors.add(" Huy Le");
-        authors.add("Minh Duc");
+        authors.add("Minh Nghi");
         Book book   = new Book(3,"chuyen tinh anh va em","teen",25.2f,authors);
 
         mediae.add(dvd);
